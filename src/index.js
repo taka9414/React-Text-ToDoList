389709922df438a15480b84c9f544be4172d5f6a
taka9414 <78952377+taka9414@users.noreply.github.com>
2021-03-04 12:54:11 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDom from "react-dom";
 
-import { App } from "./App";
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは</h1>
+      <p>おげんきですか。</p>
+    </>
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
